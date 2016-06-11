14261608d7f2f21db9de6b972969238d2aa5164e
Denys Vuika <denis.vuyka@gmail.com>
2016-06-11 14:03:11 +0100

    setup unit testing and coverage

---
+++ test/test.js
@@ -0,0 +1,10 @@
+const expect = require('expect.js');
+const Server = require('../lib/server');
+
+describe('Server', () => {
+    it('should create defaults', () => {
+        let server = new Server(null);
+        let settings = server.defaultSettings();
+        expect(settings).not.to.be(undefined);
+    });
+});
