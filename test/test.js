91183f2aab7a2bfb79cba50962b687d56db02332
Denys Vuika <denis.vuyka@gmail.com>
2016-06-12 19:06:58 +0100

    unit tests and coverage setup

--- test/test.js
+++ test/test.js
@@ -1,12 +1,111 @@
 'use strict';
 
-const expect = require('expect.js');
+const chai = require('chai');
+const expect = chai.expect;
 const Server = require('../lib/server');
+const EventEmitter = require('events').EventEmitter;
+const tinylr = require('tiny-lr');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+chai.use(sinonChai);
 
 describe('Server', () => {
+    let server;
+    let clock;
+
+    beforeEach(() => {
+        server = new Server();
+        clock = sinon.useFakeTimers(Date.now());
+    });
+
+    afterEach(() => {
+        clock.restore();
+    });
+
     it('should create defaults', () => {
-        let server = new Server(null);
         let settings = server.defaultSettings();
-        expect(settings).not.to.be(undefined);
+        expect(settings).to.not.be.undefined;
+    });
+
+    it('should log on watched file added', () => {
+        let emitter = new EventEmitter();
+        let spy = sinon.spy(server, 'log');
+        server.setupWatcher(emitter);
+        emitter.emit('add');
+        expect(spy).to.have.been.called;
+    });
+
+    it('should log on watched file changed', () => {
+        let emitter = new EventEmitter();
+        let spy = sinon.spy(server, 'log');
+        server.setupWatcher(emitter);
+        emitter.emit('change');
+        expect(spy).to.have.been.called;
+    });
+
+    it('should log on watched file unlinked', () => {
+        let emitter = new EventEmitter();
+        let spy = sinon.spy(server, 'log');
+        server.setupWatcher(emitter);
+        emitter.emit('unlink');
+        expect(spy).to.have.been.called;
+    });
+
+    it('should require config to log', () => {
+        expect(server.log('message')).to.be.false;
+    });
+
+    it('should require verbose mode to log', () => {
+        let stub = sinon.stub(console, 'log');
+
+        server.config.verbose = false;
+        expect(server.log('message')).to.be.false;
+        expect(console.log).to.not.be.called;
+
+        server.config.verbose = true;
+        expect(server.log('message')).to.be.true;
+        expect(console.log).to.be.called;
+
+        stub.restore();
+    });
+
+    it('should require message to log', () => {
+        let stub = sinon.stub(console, 'log');
+
+        server.config.verbose = true;
+        expect(server.log('message')).to.be.true;
+        expect(server.log(null)).to.be.false;
+        expect(console.log).to.have.been.calledOnce;
+
+        stub.restore();
+    });
+
+    it('should raise live reload for path', () => {
+        let stub = sinon.stub(tinylr, 'changed');
+        let watcher = new EventEmitter();
+        server.setupWatcher(watcher);
+
+        let path = 'some/path';
+        clock.tick(200);
+        watcher.emit('change', path);
+
+        expect(tinylr.changed).to.have.been.calledWith(path);
+        stub.restore();
+    });
+
+    it('should throttle live reload events', () => {
+        let stub = sinon.stub(tinylr, 'changed');
+        let watcher = new EventEmitter();
+        server.setupWatcher(watcher);
+
+        let path = 'some/path';
+        clock.tick(200);
+        watcher.emit('change', path);
+        watcher.emit('change', path);
+        watcher.emit('change', path);
+
+        expect(tinylr.changed).to.have.been.calledOnce;
+        expect(tinylr.changed).to.have.been.calledWith(path);
+        stub.restore();
     });
 });
